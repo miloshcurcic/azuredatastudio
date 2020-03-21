21ab92248c96009b9dae3526ec50666c5684bec2
Anthony Dresser <andresse@microsoft.com>
2020-03-21 15:21:29 -0700

    working inital

--- extensions/sqlite/src/index.ts
+++ extensions/sqlite/src/index.ts
@@ -4,9 +4,14 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
-import * as sqlite from 'sqlite3';
+import * as azdata from 'azdata';
+import { ConnectionProvider } from './connectionProvider';
+import { QueryProvider } from './queryProvider';
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
+	const connectionProvider = new ConnectionProvider();
+	context.subscriptions.push(azdata.dataprotocol.registerConnectionProvider(connectionProvider));
+	context.subscriptions.push(azdata.dataprotocol.registerQueryProvider(new QueryProvider(connectionProvider)));
 }
 
 export async function deactivate(): Promise<any> {
